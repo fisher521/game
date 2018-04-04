37090c6a8e5d9f63e5d6fdd91ddcaabc4e745c3b
fisher521 <fisher521.fs@gmail.com>
2018-04-04 13:02:50 -0400

    Add files via upload

---
+++ LiarsDice.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+import java.util.Random;
+/**
+ * Write a description of class HighLow1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LiarsDice
+{   
+    public static void main (String [] args) {
+        Random generator = new Random();
+        Scanner reader = new Scanner(System.in);
+        boolean play = true;
+        System.out.println ("Welcome to Liar's Dice!");
+        while (play == true) {
+            int r1 = generator.nextInt(6) + 1;
+            int r2 = generator.nextInt(6) + 1;
+            System.out.println ("Roll: " + r1 + r2);
+            while (true) {
+                System.out.println ("Play again? (y/n)");
+                String response = reader.nextLine();
+                if (response.equals("y")){
+                    break;
+                }
+                else if (response.equals("n")){
+                    play = false;
+                    break;
+                }
+            }
+        }
+    }
+}
