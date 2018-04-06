4a637378053ddb7fbbc6c0384f04f2086a8f18db
fisher521 <fisher521.fs@gmail.com>
2018-04-06 11:37:33 -0400

    singleplayer & multiplayer template

--- LiarsDice.java
+++ LiarsDice.java
@@ -1,6 +1,5 @@
 
 import java.util.Scanner;
-import java.util.Random;
 /**
  * Write a description of class HighLow1 here.
  *
@@ -10,25 +9,7 @@ import java.util.Random;
 public class LiarsDice
 {
     public static void main (String [] args) {
-        Random generator = new Random();
-        Scanner reader = new Scanner(System.in);
-        boolean play = true;
         System.out.println ("Welcome to Liar's Dice!");
-        while (play == true) {
-            int r1 = generator.nextInt(6) + 1;
-            int r2 = generator.nextInt(6) + 1;
-            System.out.println ("Roll: " + r1 + r2);
-            while (true) {
-                System.out.println ("Play again? (y/n)");
-                String response = reader.nextLine();
-                if (response.equals("y")){
-                    break;
-                }
-                else if (response.equals("n")){
-                    play = false;
-                    break;
-                }
-            }
-        }
+        
     }
 }
---
+++ SinglePlayer.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Random;
+/**
+ * Write a description of class SinglePlayer here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SinglePlayer
+{
+    public static void main (String [] args) {
+        Random generator = new Random();
+        Scanner reader = new Scanner(System.in);
+        boolean play = true;
+        while (play == true) {
+            int r1 = generator.nextInt(6) + 1;
+            int r2 = generator.nextInt(6) + 1;
+            System.out.println ("Roll: " + r1 + r2);
+            while (true) {
+                System.out.println ("Play again? (y/n)");
+                String response = reader.nextLine();
+                if (response.equals("y")){
+                    break;
+                }
+                else if (response.equals("n")){
+                    play = false;
+                    break;
+                }
+            }
+        }
+    }
+}
