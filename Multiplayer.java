b73d7f832b4b216132e4251d0df78890d6d51449
fisher521 <fisher521.fs@gmail.com>
2018-05-08 10:30:35 -0400

    changed code to use classes, not working yet

--- Multiplayer.java
+++ Multiplayer.java
@@ -12,177 +12,95 @@ public class Multiplayer {
     public static Scanner reader = new Scanner(System.in);
     public static void main (String [] args) {
         System.out.println();
-        int p1NumberOfDice = 5;
-        int p2NumberOfDice = 5;
+        System.out.println("How many players? (1-6): ");
+        int numberOfPlayers = reader.nextInt();
+        reader.nextLine();
+        
+        Player[] player = new Player[6];
+        
+        for (int i = 0; i < numberOfPlayers; i++) {
+            System.out.println("Player " + i + ", what is your name? ");
+            player[i] = new Player(i, reader.nextLine());
+        }
         
         System.out.println("Player 1, what is your name? ");
-        String player1 = reader.nextLine();
+        Player player1 = new Player(1, reader.nextLine());
         
         System.out.println("Player 2, what is your name? ");
-        String player2 = reader.nextLine();
+        Player player2 = new Player(2, reader.nextLine());
         
-        while (p1NumberOfDice > 0 && p2NumberOfDice > 0) {
+        while (player1.getNumberOfDice() > 0 && player2.getNumberOfDice() > 0) {
             System.out.println();
             
-            //dice rolls for player 1
-            rollDicePrompt(player1);
-            int [] player1Roll = new int[5];
-            for(int i = 0; i < p1NumberOfDice; i++) {
-                player1Roll[i] = diceRoll();
+            for (int i = 0; i < numberOfPlayers; i++) {                
+                player[i].printRolls("Rolls: ");
+                System.out.println();
+                Multiplayer.pause("continue");
             }
-            System.out.print("Rolls: ");
-            for(int i = 1; i <= p1NumberOfDice; i++) {     
-                System.out.print(player1Roll[i - 1]);
-                if (p1NumberOfDice - i >= 1) {
-                    System.out.print(", ");
-                } 
-            }    
-            System.out.println();
-            pause("continue");
-            
-            //dice rolls for player 2
-            rollDicePrompt(player2);
-            int [] player2Roll = new int[5];
-            for(int i = 0; i < p2NumberOfDice; i++) {
-                player2Roll[i] = diceRoll();
-            }
-            System.out.print("Rolls: ");
-            for(int i = 1; i <= p2NumberOfDice; i++) {     
-                System.out.print(player2Roll[i - 1]);
-                if (p2NumberOfDice - i >= 1) { 
-                    System.out.print(", ");
-                }
-            }    
-            System.out.println();
-            pause("continue");
             
             //turn 1
-            System.out.println(player1.toUpperCase() + "'S TURN");
+            System.out.println(player1.getName().toUpperCase() + "'S TURN");
             System.out.println("Bid a quantity and face value.\nQuantity: ");
             int quantity = reader.nextInt();
             reader.nextLine();
             System.out.println("Face Value: ");
             int faceValue = reader.nextInt();
             reader.nextLine();
-            System.out.println(player1 + " bidded " + quantity + " " + faceValue + "'s.");
+            System.out.println(player1.getName() + " bidded " + quantity + " " + faceValue + "'s.");
             
             //all subsequent turns
-            int player = 2;
+            int turn = 2;
             boolean round = true;
             while (round) {
-                if (player == 2) {
-                    System.out.println("\n" + player2.toUpperCase() + "'S TURN");
-                    while (true) {
-                        System.out.println("Would you like to challenge or make a higher bid? (c/b)");              
-                        String decision = reader.nextLine();
-                        if (decision.equals("c")){
-                            int matchingFaces = 0;
-                            System.out.print("\n" + player1 + "'s Rolls: ");
-                            for(int i = 1; i <= p1NumberOfDice; i++) {     
-                                System.out.print(player1Roll[i - 1]);
-                                if (p1NumberOfDice - i >= 1) {
-                                    System.out.print(", ");
-                                } 
-                            }
-                            System.out.print("\n" + player2 + "'s Rolls: ");
-                            for(int i = 1; i <= p2NumberOfDice; i++) {     
-                                System.out.print(player2Roll[i - 1]);
-                                if (p2NumberOfDice - i >= 1) {
-                                    System.out.print(", ");
-                                } 
-                            }
-                            for (int i = 0; i < p1NumberOfDice; i++) {
-                                if (player1Roll[i] == faceValue) {
-                                    matchingFaces++;
-                                }
-                            }
-                            for (int i = 0; i < p2NumberOfDice; i++) {
-                                if (player2Roll[i] == faceValue) {
-                                    matchingFaces++;
-                                }
-                            }
-                            System.out.println("\nMatching faces: " + matchingFaces);
-                            if (matchingFaces >= quantity) {
-                                p2NumberOfDice--;
-                                System.out.println(player2 + " has lost a die and now has " + p2NumberOfDice + " dice!");
-                            }
-                            else {
-                                p1NumberOfDice--;
-                                System.out.println(player1 + " has lost a die and now has " + p1NumberOfDice + " dice!");
-                            }
-                            round = false;
-                            System.out.println();
-                            if (p1NumberOfDice > 0 && p2NumberOfDice > 0) {
-                                pause("begin new round");
-                            }
-                            player = 1;
-                            break;
-                        }
-                        if (decision.equals("b")) {
-                            while(true){
-                                System.out.println("Would you like to enter a higher face value or a higher quantity? (f/q)");
-                                String bidDecision = reader.nextLine();
-                                if(bidDecision.equals("f")){
-                                    faceValue = higherValue("face value", faceValue);
-                                    break;
-                                }
-                                else if(bidDecision.equals("q")){
-                                    quantity = higherValue("quantity", quantity);
-                                    break;
-                                }
-                            }
-                            System.out.println(player2 + " bidded " + quantity + " " + faceValue + "'s.");
-                            player = 1;
-                            break;
-                        }
-                    }
+                if (turn == 2) {
+                    player2.turn();
                 }
-                else if (player == 1) {
-                    System.out.println("\n" + player1.toUpperCase() + "'S TURN");
+                else if (turn == 1) {
+                    System.out.println("\n" + player1.getName().toUpperCase() + "'S TURN");
                     while (true) {
                         System.out.println("Would you like to challenge or make a higher bid? (c/b)");              
                         String decision = reader.nextLine();
                         if (decision.equals("c")){
                             int matchingFaces = 0;
-                            System.out.print("\n" + player1 + "'s Rolls: ");
-                            for(int i = 1; i <= p1NumberOfDice; i++) {     
+                            System.out.print("\n" + player1.getName() + "'s Rolls: ");
+                            for(int i = 1; i <= player1.getNumberOfDice(); i++) {     
                                 System.out.print(player1Roll[i - 1]);
-                                if (p1NumberOfDice - i >= 1) {
+                                if (player1.getNumberOfDice() - i >= 1) {
                                     System.out.print(", ");
                                 } 
                             }
-                            System.out.print("\n" + player2 + "'s Rolls: ");
-                            for(int i = 1; i <= p2NumberOfDice; i++) {     
+                            System.out.print("\n" + player2.getName() + "'s Rolls: ");
+                            for(int i = 1; i <= player2.getNumberOfDice(); i++) {     
                                 System.out.print(player2Roll[i - 1]);
-                                if (p2NumberOfDice - i >= 1) {
+                                if (player2.getNumberOfDice() - i >= 1) {
                                     System.out.print(", ");
                                 } 
                             }
-                            for (int i = 0; i < p1NumberOfDice; i++) {
+                            for (int i = 0; i < player1.getNumberOfDice(); i++) {
                                 if (player1Roll[i] == faceValue) {
                                     matchingFaces++;
                                 }
                             }
-                            for (int i = 0; i < p2NumberOfDice; i++) {
+                            for (int i = 0; i < player2.getNumberOfDice(); i++) {
                                 if (player2Roll[i] == faceValue) {
                                     matchingFaces++;
                                 }
                             }
                             System.out.println("\nMatching faces: " + matchingFaces);
                             if (matchingFaces >= quantity) {
-                                p1NumberOfDice--;
-                                System.out.println(player1 + " has lost a die and now has " + p1NumberOfDice + " dice!");
+                                player1.loseDice();
+                                System.out.println(player1.getName() + " has lost a die and now has " + player1.getNumberOfDice() + " dice!");
                             }
                             else {
-                                p2NumberOfDice--;
-                                System.out.println(player2 + " has lost a die and now has " + p2NumberOfDice + " dice!");
+                                player2.loseDice();
+                                System.out.println(player2.getName() + " has lost a die and now has " + player2.getNumberOfDice() + " dice!");
                             }
                             round = false;
                             System.out.println();
-                            if (p1NumberOfDice > 0 && p2NumberOfDice > 0) {
+                            if (player1.getNumberOfDice() > 0 && player2.getNumberOfDice() > 0) {
                                 pause("begin new round");
                             }
-                            player = 2;
+                            turn = 2;
                             break;
                         }
                         if (decision.equals("b")) {
@@ -198,19 +116,20 @@ public class Multiplayer {
                                     break;
                                 }
                             }
-                            System.out.println(player1 + " bidded " + quantity + " " + faceValue + "'s.");
-                            player = 2;
+                            System.out.println(player1.getName() + " bidded " + quantity + " " + faceValue + "'s.");
+                            turn = 2;
                             break;
+                            
                         }
                     }
                 }
             }
         }
-        if (p1NumberOfDice == 0) {
-            System.out.println(player2 + " wins!");
+        if (player1.getNumberOfDice() == 0) {
+            System.out.println(player2.getName() + " wins!");
         }
-        else if (p2NumberOfDice == 0) {
-            System.out.println(player1 + " wins!");
+        else if (player2.getNumberOfDice() == 0) {
+            System.out.println(player1.getName() + " wins!");
         }
     }
     public static void rollDicePrompt(String player) {
@@ -237,6 +156,42 @@ public class Multiplayer {
                 return newValue;
             }
         }
+    }
+    public static void printRolls (String player) {
+    }
+    public void challenge(int faceValue, int quantity, int numberOfPlayers) {
+        //prints rolls
+        for (int player = 0; player < numberOfPlayers; player++) {
+            printRolls("\n" + player[1].getName() + "'s Rolls: ");
+        }
+
+        //checks rolls
+        for (int i = 0; i < player1.getNumberOfDice(); i++) {
+            if (player1Roll[i] == faceValue) {
+                matchingFaces++;
+            }
+        }
+        for (int i = 0; i < player2.getNumberOfDice(); i++) {
+            if (player2Roll[i] == faceValue) {
+                matchingFaces++;
+            }
+        }
+        System.out.println("\nMatching faces: " + matchingFaces);
+        if (matchingFaces >= quantity) {
+            player1.loseDice();
+            System.out.println(player1.getName() + " has lost a die and now has " + player1.getNumberOfDice() + " dice!");
+        }
+        else {
+            player2.loseDice();
+            System.out.println(player2.getName() + " has lost a die and now has " + player2.getNumberOfDice() + " dice!");
+        }
+        round = false;
+        System.out.println();
+        if (player1.getNumberOfDice() > 0 && player2.getNumberOfDice() > 0) {
+            pause("begin new round");
+        }
+        turn = 2;
+        break;
     }
 }
 
